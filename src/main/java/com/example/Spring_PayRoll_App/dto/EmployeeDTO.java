864fcfe1e547e154c52acfe5a7be6f6aab32bf67
Code-CLan-Community <veergupta0801@gmail.com>
2025-02-28 10:38:19 +0530

    [Rishabh Gupta] Add. auto generate getters and setters using Lambok Library

--- src/main/java/com/example/Spring_PayRoll_App/dto/EmployeeDTO.java
+++ src/main/java/com/example/Spring_PayRoll_App/dto/EmployeeDTO.java
@@ -1,21 +1,13 @@
 package com.example.Spring_PayRoll_App.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data  // Generates Getters, Setters, equals(), hashCode(), toString()
+@NoArgsConstructor // Generates Default Constructor
+@AllArgsConstructor // Generates Constructor with All Fields
 public class EmployeeDTO {
     private String name;
     private double salary;
-
-    // Constructors
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    // Getters & Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public double getSalary() { return salary; }
-    public void setSalary(double salary) { this.salary = salary; }
 }
